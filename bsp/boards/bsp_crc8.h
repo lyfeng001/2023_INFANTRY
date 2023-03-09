8b7526dbc0c1367908d01985e4633dd6c0fd3df4
XiaoYoung <xy_23@outlook.com>
2023-03-09 20:34:46 +0800

    更改ignore

---
+++ bsp/boards/bsp_crc8.h
@@ -0,0 +1,14 @@
+/*
+* @Author: TJSP_2022_TY
+* @Date:   2021-11-30
+* @Last Modified by:   
+* @Last Modified time:  
+* @brief: 移植自20年老代码，Hal库应该有，可以后期迭代优化代码
+*/
+#ifndef BSP_CRC8_H
+#define BSP_CRC8_H
+#include "struct_typedef.h"
+
+
+
+#endif
